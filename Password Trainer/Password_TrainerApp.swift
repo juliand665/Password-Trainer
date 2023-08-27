2a3f924d8aa8f779aa07c6233c0ac526cb0d4494
Julian Dunskus <julian.dunskus@gmail.com>
2023-08-27 21:14:00 +0200

    Initial Commit

---
+++ Password Trainer/Password_TrainerApp.swift
@@ -0,0 +1,12 @@
+// Created by Julian Dunskus
+
+import SwiftUI
+
+@main
+struct Password_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
